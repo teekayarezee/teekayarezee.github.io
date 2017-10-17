947e5da78dce1cc23283682cb68dc795916252d7
irobot335 <sysfader@gmail.com>
2017-10-17 21:16:14 +0100

    Release 2

--- fc/scriptMin.js
+++ fc/scriptMin.js
@@ -12,19 +12,6 @@
 // Author:          TkrZ / Irobot335
 //                  @TkrZ
 
-
-// Preliminary functions
-function setTick(n, m) { "use strict"; var o, step = 6400; while (m <= step) { step -= n; } o = step + n - m; if (o < 1) { o = n; } return o; }
-
-function getSecs(n, m, s) { "use strict";
-var tick = setTick(n, m), 
-secsFromHours = Math.floor(((tick * 60) - s) / 3600),
-a = secsFromHours === 12 ? 0 : secsFromHours,
-b = Math.floor((((tick * 60) - s) % 3600) / 60);
-return ((a * 3600) + (b * 60) + ( ((tick * 60) - s) % 60 )) * 1e3; }
-
-function updateElement(c, h, m, s) { "use strict"; document.getElementById(c).innerHTML = h + ':' + m + ':' + s; }
-
 // Main Function
 function countDown() {
     "use strict";
@@ -36,54 +23,64 @@ function countDown() {
         timeHour = (timeDate.getUTCHours() < 12) ? timeDate.getUTCHours() + 24 : (timeDate.getUTCHours() === 0) ? 12 : timeDate.getUTCHours(),
         timeMin = timeDate.getUTCMinutes(),
         timeInMins = (timeHour * 60) + timeMin,
-        hh = timeHour * 60 * 60,
-        mm = timeMin === 0 ? 1 : timeMin * 60,
-        tt = Math.floor((hh + mm + timeSec) / 30) + -405,
+        hh = timeHour * 3600,
+        //mm = timeMin === 0 ? 1 : timeMin * 60,
+        tt = Math.floor((hh + timeMin * 60 + timeSec) / 30) + -405,
+        
+        mathHours = function (m) { return ('0' + (Math.floor(m / 36e5))).slice(-2); },
+        mathMins = function (m) { return ('0' + (Math.floor((m % 36e5) / 6e4))).slice(-2); },
+        setTick = function (n) { var o, step = 6400; while (timeInMins <= step) { step -= n; } o = step + n - timeInMins; if (o < 1) { o = n; } return o * 60; },
+        getSecs = function (n) { var tick = setTick(n), secsFromHours = Math.floor((tick - timeSec) / 3600), a = secsFromHours === 12 ? 0 : secsFromHours, b = Math.floor(((tick - timeSec) % 3600) / 60);
+                                return ((a * 3600) + (b * 60) + ((tick - timeSec) % 60)) * 1e3; },
+        updateEl = function (e, c) { document.getElementById(e).innerHTML = c; },
     
         //Spirit Tree 320 min cycles
-        sTreeCDSecs = getSecs(320, timeInMins, timeSec),
+        sTreeCDSecs = getSecs(320),
         //Cacti 80 min cycles
-        cactiCDSecs = getSecs(80, timeInMins, timeSec),
+        cactiCDSecs = getSecs(80),
         //Fruit tree 160 min cycles
-        fTreeCDSecs = getSecs(160, timeInMins, timeSec),
+        fTreeCDSecs = getSecs(160),
         //Reg tree 40 min cycles
-        rTreeCDSecs = getSecs(40, timeInMins, timeSec),
-        //Herb  20 min cycles
-        herbCDSecs = getSecs(20, timeInMins, timeSec),
-        //Allotment  10 min cycles
-        allotCDSecs = getSecs(10, timeInMins, timeSec),
+        rTreeCDSecs = getSecs(40),
+        //Herb 20 min cycles
+        herbCDSecs = getSecs(20),
+        //Allotment 10 min cycles
+        allotCDSecs = getSecs(10),
         
-        //millis = ((timeHour * 3600) * 1e3) + ((timeMin * 60) * 1e3) + (timeSec * 1e3) + 1e3,
-        millis1 = (sTreeCDSecs > 192e5 || sTreeCDSecs < 0) ? 192e5 : sTreeCDSecs,
-        millis2 = (fTreeCDSecs > 96e5 || fTreeCDSecs < 0) ? 96e5 : fTreeCDSecs,
-        millis3 = (cactiCDSecs > 48e5 || cactiCDSecs < 0) ? 48e5 : cactiCDSecs,
-        millis4 = (rTreeCDSecs > 24e5 || rTreeCDSecs < 0) ? 24e5 : rTreeCDSecs,
-        millis5 = (herbCDSecs > 12e5 || herbCDSecs < 0) ? 12e5 : herbCDSecs,
-        millis6 = (allotCDSecs > 6e5 || allotCDSecs < 0) ? 6e5 : allotCDSecs,
+        millis = ((timeHour * 3600) * 1e3) + ((timeMin * 60) * 1e3) + (timeSec * 1e3) + 1e3,
+        milli = function (n, t) { return (t > n * 1e5 || t * 1e5 < 0) ? n * 1e5 : t; },
+        millis1 = milli(192, sTreeCDSecs),
+        millis2 = milli(96, fTreeCDSecs),
+        millis3 = milli(48, cactiCDSecs),
+        millis4 = milli(24, rTreeCDSecs),
+        millis5 = milli(12, herbCDSecs),
+        millis6 = milli(6, allotCDSecs),
+        
         secs1 = ('0' + (Math.floor((millis1 % 6e4) / 1e3))).slice(-2),
-        mins1 = ('0' + (Math.floor((millis1 % 36e5) / 6e4))).slice(-2),
-        hours1 = ('0' + (Math.floor(millis1 / 36e5))).slice(-2),
-        mins2 = ('0' + (Math.floor((millis2 % 36e5) / 6e4))).slice(-2),
-        hours2 = ('0' + (Math.floor(millis2 / 36e5))).slice(-2),
-        mins3 = ('0' + (Math.floor((millis3 % 36e5) / 6e4))).slice(-2),
-        hours3 = ('0' + (Math.floor(millis3 / 36e5))).slice(-2),
-        mins4 = ('0' + (Math.floor((millis4 % 36e5) / 6e4))).slice(-2),
-        hours4 = ('0' + (Math.floor(millis4 / 36e5))).slice(-2),
-        mins5 = ('0' + (Math.floor((millis5 % 36e5) / 6e4))).slice(-2),
-        hours5 = ('0' + (Math.floor(millis5 / 36e5))).slice(-2),
-        mins6 = ('0' + (Math.floor((millis6 % 36e5) / 6e4))).slice(-2),
-        hours6 = ('0' + (Math.floor(millis6 / 36e5))).slice(-2);
+        hours1 = mathHours(millis1),
+        mins1 = mathMins(millis1),
+        hours2 = mathHours(millis2),
+        mins2 = mathMins(millis2),
+        hours3 = mathHours(millis3),
+        mins3 = mathMins(millis3),
+        hours4 = mathHours(millis4),
+        mins4 = mathMins(millis4),
+        hours5 = mathHours(millis5),
+        mins5 = mathMins(millis5),
+        hours6 = mathHours(millis6),
+        mins6 = mathMins(millis6);
     
-    updateElement("count0", hours1, mins1, secs1);
-    updateElement("count1", hours2, mins2, secs1);
-    updateElement("count2", hours3, mins3, secs1);
-    updateElement("count3", hours4, mins4, secs1);
-    updateElement("count4", hours5, mins5, secs1);
-    updateElement("count5", hours6, mins6, secs1);
-    document.getElementById('currentHour').innerHTML = timeHourD;
-    document.getElementById('currentMin').innerHTML = timeMin;
-    document.getElementById('currentSec').innerHTML = timeSec;
-    document.getElementById("am_pm").innerHTML = " " + timeAMPM;
-    document.getElementById("currentDate").innerHTML = timeDate.toString().split(' ').splice(1, 3).join(' ');
+    updateEl("count0", hours1 + ":" + mins1 + ":" + secs1);
+    updateEl("count1", hours2 + ":" + mins2 + ":" + secs1);
+    updateEl("count2", hours3 + ":" + mins3 + ":" + secs1);
+    updateEl("count3", hours4 + ":" + mins4 + ":" + secs1);
+    updateEl("count4", hours5 + ":" + mins5 + ":" + secs1);
+    updateEl("count5", hours6 + ":" + mins6 + ":" + secs1);
+    updateEl("currentHour", timeHourD);
+    updateEl("currentMin", timeMin);
+    updateEl("currentSec", timeSec);
+    updateEl("am_pm", timeAMPM);
+    updateEl("currentDate", timeDate.toString().split(' ').splice(1, 3).join(' '));
+    updateEl("currentHour", timeHourD);
     document.getElementById("icon").style.backgroundPosition = "-" + tt + "px 0px";
 }
