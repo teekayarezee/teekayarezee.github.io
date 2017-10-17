2db9014e1ddf52f13e4a2d0bbc60e888f28afdab
irobot335 <sysfader@gmail.com>
2017-10-17 23:17:41 +0100

    Release Final 1

--- fc/scriptMin.js
+++ fc/scriptMin.js
@@ -19,54 +19,49 @@ function countDown() {
     var timeDate = new Date(),
         timeAMPM = (timeDate.getUTCHours() >= 12) ? "PM" : "AM",
         timeSec = timeDate.getUTCSeconds(),
-        timeHourD = timeDate.getUTCHours(),
-        timeHour = (timeDate.getUTCHours() < 12) ? timeDate.getUTCHours() + 24 : (timeDate.getUTCHours() === 0) ? 12 : timeDate.getUTCHours(),
+        timeHour = (timeDate.getUTCHours() < 12) ? timeDate.getUTCHours() + 24 : timeDate.getUTCHours(),
         timeMin = timeDate.getUTCMinutes(),
         timeInMins = (timeHour * 60) + timeMin,
         hh = timeHour * 3600,
         //mm = timeMin === 0 ? 1 : timeMin * 60,
         tt = Math.floor((hh + timeMin * 60 + timeSec) / 30) + -405,
         
-        mathHours = function (m) { return ('0' + (Math.floor(m / 36e5))).slice(-2); },
-        mathMins = function (m) { return ('0' + (Math.floor((m % 36e5) / 6e4))).slice(-2); },
-        setTick = function (n) { var o, step = 6400; while (timeInMins <= step) { step -= n; } o = step + n - timeInMins; if (o < 1) { o = n; } return o * 60; },
-        getSecs = function (n) { var tick = setTick(n), secsFromHours = Math.floor((tick - timeSec) / 3600), a = secsFromHours === 12 ? 0 : secsFromHours, b = Math.floor(((tick - timeSec) % 3600) / 60);
-                                return ((a * 3600) + (b * 60) + ((tick - timeSec) % 60)) * 1e3; },
-        updateEl = function (e, c) { document.getElementById(e).innerHTML = c; },
-    
-        //Spirit Tree 320 min cycles
-        sTreeCDSecs = getSecs(320),
-        //Cacti 80 min cycles
-        cactiCDSecs = getSecs(80),
-        //Fruit tree 160 min cycles
-        fTreeCDSecs = getSecs(160),
-        //Reg tree 40 min cycles
-        rTreeCDSecs = getSecs(40),
-        //Herb 20 min cycles
-        herbCDSecs = getSecs(20),
-        //Allotment 10 min cycles
-        allotCDSecs = getSecs(10),
-        
-        millis = ((timeHour * 3600) * 1e3) + ((timeMin * 60) * 1e3) + (timeSec * 1e3) + 1e3,
+        mathHours = function (m) { return ("0" + (Math.floor(m / 36e5))).slice(-2); },
+        mathMins = function (m) { return ("0" + (Math.floor((m % 36e5) / 6e4))).slice(-2); },
+        getMSecs = function (n) {var tick, step = 6400; while (timeInMins <= step) { step -= n; } tick = step + n - timeInMins; if (tick < 1) { tick = n; } tick *= 60;
+                                var hour = Math.floor((tick - timeSec) / 3600), min = Math.floor(((tick - timeSec) % 3600) / 60);
+                                return ((hour * 3600) + (min * 60) + ((tick - timeSec) % 60)) * 1e3; },
         milli = function (n, t) { return (t > n * 1e5 || t * 1e5 < 0) ? n * 1e5 : t; },
-        millis1 = milli(192, sTreeCDSecs),
-        millis2 = milli(96, fTreeCDSecs),
-        millis3 = milli(48, cactiCDSecs),
-        millis4 = milli(24, rTreeCDSecs),
-        millis5 = milli(12, herbCDSecs),
-        millis6 = milli(6, allotCDSecs),
+        updateEl = function (e, c) { document.getElementById(e).innerHTML = c; },
         
-        secs1 = ('0' + (Math.floor((millis1 % 6e4) / 1e3))).slice(-2),
+        //Spirit Tree 320 min cycles
+        millis1 = milli(192, getMSecs(320)),
         hours1 = mathHours(millis1),
         mins1 = mathMins(millis1),
+        secs1 = ('0' + (Math.floor((millis1 % 6e4) / 1e3))).slice(-2),
+        
+        //fTreeCDSecs = getMSecs(160),
+        millis2 = milli(96, getMSecs(160)),
         hours2 = mathHours(millis2),
         mins2 = mathMins(millis2),
+        
+        //Cacti 80 min cycles
+        millis3 = milli(48, getMSecs(80)),
         hours3 = mathHours(millis3),
         mins3 = mathMins(millis3),
+        
+        //Reg tree 40 min cycles
+        millis4 = milli(24, getMSecs(40)),
         hours4 = mathHours(millis4),
         mins4 = mathMins(millis4),
+        
+        //Herb 20 min cycles
+        millis5 = milli(12, getMSecs(20)),
         hours5 = mathHours(millis5),
         mins5 = mathMins(millis5),
+        
+        //Allotment 10 min cycles
+        millis6 = milli(6, getMSecs(10)),
         hours6 = mathHours(millis6),
         mins6 = mathMins(millis6);
     
@@ -76,11 +71,10 @@ function countDown() {
     updateEl("count3", hours4 + ":" + mins4 + ":" + secs1);
     updateEl("count4", hours5 + ":" + mins5 + ":" + secs1);
     updateEl("count5", hours6 + ":" + mins6 + ":" + secs1);
-    updateEl("currentHour", timeHourD);
+    updateEl("currentHour", timeDate.getUTCHours());
     updateEl("currentMin", timeMin);
     updateEl("currentSec", timeSec);
     updateEl("am_pm", timeAMPM);
     updateEl("currentDate", timeDate.toString().split(' ').splice(1, 3).join(' '));
-    updateEl("currentHour", timeHourD);
     document.getElementById("icon").style.backgroundPosition = "-" + tt + "px 0px";
 }
--- fc/table.js
+++ fc/table.js
@@ -1,116 +1,117 @@
-$(document).ready(function() {
-  $('#tDisplay3').hide();
-  $('#tDisplay2').hide();
-  $(".row1").click(function() {
-    $(this).toggleClass("coloured");
-    $('.div-table-row1').toggleClass('coloured2');
-  });
-  $(".row2").click(function() {
-    $(this).toggleClass("coloured");
-    $('.div-table-row2').toggleClass('coloured2');
-  });
-  $(".row3").click(function() {
-    $(this).toggleClass("coloured");
-    $('.div-table-row3').toggleClass('coloured2');
-  });
-  $(".row4").click(function() {
-    $(this).toggleClass("coloured");
-    $('.div-table-row4').toggleClass('coloured2');
-  });
-  $(".row5").click(function() {
-    $(this).toggleClass("coloured");
-    $('.div-table-row5').toggleClass('coloured2');
-  });
-  $(".row6").click(function() {
-    $(this).toggleClass("coloured");
-    $('.div-table-row6').toggleClass('coloured2');
-  });
-  $(".row7").click(function() {
-    $(this).toggleClass("coloured");
-    $('.div-table-row7').toggleClass('coloured2');
-  });
-  $('.row1').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row1').toggleClass('coloured');
-      $('.div-table-row1').toggleClass('coloured2');
-    }
-  });
-  $('.div-table-col1').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row1').toggleClass('coloured');
-      $('.div-table-row1').toggleClass('coloured2');
-    }
-  });
-  $('.row2').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row2').toggleClass('coloured');
-      $('.div-table-row2').toggleClass('coloured2');
-    }
-  });
-  $('.div-table-col2').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row2').toggleClass('coloured');
-      $('.div-table-row2').toggleClass('coloured2');
-    }
-  });
-  $('.row3').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row3').toggleClass('coloured');
-      $('.div-table-row3').toggleClass('coloured2');
-    }
-  });
-  $('.div-table-col3').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row3').toggleClass('coloured');
-      $('.div-table-row3').toggleClass('coloured2');
-    }
-  });
-  $('.row4').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row4').toggleClass('coloured');
-      $('.div-table-row4').toggleClass('coloured2');
-    }
-  });
-  $('.div-table-col4').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row4').toggleClass('coloured');
-      $('.div-table-row4').toggleClass('coloured2');
-    }
-  });
-  $('.row5').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row5').toggleClass('coloured');
-      $('.div-table-row5').toggleClass('coloured2');
-    }
-  });
-  $('.div-table-col5').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row5').toggleClass('coloured');
-      $('.div-table-row5').toggleClass('coloured2');
-    }
-  });
-  $('.row6').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row6').toggleClass('coloured');
-      $('.div-table-row6').toggleClass('coloured2');
-    }
-  });
-  $('.div-table-col6').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row6').toggleClass('coloured');
-      $('.div-table-row6').toggleClass('coloured2');
-    }
-  });
-  $('.row7').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row7').toggleClass('coloured');
-      $('.div-table-row7').toggleClass('coloured2');
-    }
-  });
-  $('.div-table-col7').hover(function() {
-    if ($('#chkSelect').is(":checked")) {
-      $('.row7').toggleClass('coloured');
-      $('.div-table-row7').toggleClass('coloured2');
-    }
-  });
-})
+$(document).ready(function () {
+    "use strict";
+    $('#tDisplay3').hide();
+    $('#tDisplay2').hide();
+    $(".row1").click(function () {
+        $(this).toggleClass("coloured");
+        $('.div-table-row1').toggleClass('coloured2');
+    });
+    $(".row2").click(function () {
+        $(this).toggleClass("coloured");
+        $('.div-table-row2').toggleClass('coloured2');
+    });
+    $(".row3").click(function () {
+        $(this).toggleClass("coloured");
+        $('.div-table-row3').toggleClass('coloured2');
+    });
+    $(".row4").click(function () {
+        $(this).toggleClass("coloured");
+        $('.div-table-row4').toggleClass('coloured2');
+    });
+    $(".row5").click(function () {
+        $(this).toggleClass("coloured");
+        $('.div-table-row5').toggleClass('coloured2');
+    });
+    $(".row6").click(function () {
+        $(this).toggleClass("coloured");
+        $('.div-table-row6').toggleClass('coloured2');
+    });
+    $(".row7").click(function () {
+        $(this).toggleClass("coloured");
+        $('.div-table-row7').toggleClass('coloured2');
+    });
+    $('.row1').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row1').toggleClass('coloured');
+            $('.div-table-row1').toggleClass('coloured2');
+        }
+    });
+    $('.div-table-col1').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row1').toggleClass('coloured');
+            $('.div-table-row1').toggleClass('coloured2');
+        }
+    });
+    $('.row2').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row2').toggleClass('coloured');
+            $('.div-table-row2').toggleClass('coloured2');
+        }
+    });
+    $('.div-table-col2').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row2').toggleClass('coloured');
+            $('.div-table-row2').toggleClass('coloured2');
+        }
+    });
+    $('.row3').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row3').toggleClass('coloured');
+            $('.div-table-row3').toggleClass('coloured2');
+        }
+    });
+    $('.div-table-col3').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row3').toggleClass('coloured');
+            $('.div-table-row3').toggleClass('coloured2');
+        }
+    });
+    $('.row4').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row4').toggleClass('coloured');
+            $('.div-table-row4').toggleClass('coloured2');
+        }
+    });
+    $('.div-table-col4').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row4').toggleClass('coloured');
+            $('.div-table-row4').toggleClass('coloured2');
+        }
+    });
+    $('.row5').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row5').toggleClass('coloured');
+            $('.div-table-row5').toggleClass('coloured2');
+        }
+    });
+    $('.div-table-col5').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row5').toggleClass('coloured');
+            $('.div-table-row5').toggleClass('coloured2');
+        }
+    });
+    $('.row6').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row6').toggleClass('coloured');
+            $('.div-table-row6').toggleClass('coloured2');
+        }
+    });
+    $('.div-table-col6').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row6').toggleClass('coloured');
+            $('.div-table-row6').toggleClass('coloured2');
+        }
+    });
+    $('.row7').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row7').toggleClass('coloured');
+            $('.div-table-row7').toggleClass('coloured2');
+        }
+    });
+    $('.div-table-col7').hover(function () {
+        if ($('#chkSelect').is(":checked")) {
+            $('.row7').toggleClass('coloured');
+            $('.div-table-row7').toggleClass('coloured2');
+        }
+    });
+});
