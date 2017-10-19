9279ee9642b115cf6007e33c95a669aa78105822
irobot335 <sysfader@gmail.com>
2017-10-20 00:05:42 +0100

    px fix

--- fc/script.js
+++ fc/script.js
@@ -9,14 +9,11 @@
 var px;
 function countDown() {
     "use strict";
-    var a = 10, i, p, tick,
-        d = new Date(),
-        h = d.getUTCHours(), h6 = d.getUTCHours() * 60, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h6 + m,
-        math = function (e) { return ("0" + Math.floor(e)).slice(-2); };
-    for (i = 6; i > 0; i -= 1) { p = 6400; while (t <= p) { p -= a; } tick = p + a - t; if (tick < 1) { tick = a; } a *= 2; p = tick * 60 - s;
-                                document.getElementById("count" + i).innerHTML = math(p / 3600) + ":" + math((p % 3600) / 60) + ":" + math(60 - s); }
+    var a = 10, i, p, k, d = new Date(), h = d.getUTCHours(), m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h * 60 + m, f = function (e) { return ("0" + Math.floor(e)).slice(-2); };
+    for (i = 6; i > 0; i -= 1) { p = 6400; while (t <= p) { p -= a; } k = p + a - t; if (k < 1) { k = a; } a *= 2; p = k * 60 - s;
+                                document.getElementById("count" + i).innerHTML = f(p / 3600) + ":" + f((p % 3600) / 60) + ":" + f(60 - s); }
     document.getElementById("currentDate").innerHTML = d.toUTCString();
-    px = Math.floor((((h < 12 ? h + 24 : h) + m) * 60 + s) / 30) + -405;
+    px = Math.floor((((h < 12 ? h + 24 : h) * 60 + m) * 60 + s) / 30) + -405;
 }
 
 document.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
