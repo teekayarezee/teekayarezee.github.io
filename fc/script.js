daa6309b24bafc65f739a551cea01442d6d18a32
irobot335 <sysfader@gmail.com>
2017-10-19 22:16:31 +0100

    Release Final 8

--- fc/script.js
+++ fc/script.js
@@ -1,31 +1,22 @@
 /* eslint-env browser */
 /* exported px */
-// _____ _        _____
-// |_   _| | ___ _|__  /
-//   | | | |/ / '__|/ / 
-//   | | |   <| |  / /_ 
-//   |_| |_|\_\_| /____|
-//
-// Farming Tick Timer/Clock/Calculator - PHP to HTML/JS Re-write
-// Many thanks to "Indecent Act" for permission
-//
-// Version 2.0c
-// Original Author: "Indecent Act" / @RSLinkify
-// Author:          "TkrZ" / @TkrZ
+//  _____ _        _____  |  Farming Tick Timer/Clock/Calculator - PHP to HTML/JS Re-write
+// |_   _| | ___ _|__  /  |  Version 2.5a
+//   | | | |/ / '__|/ /   |  Original Author: "Indecent Act" / @RSLinkify
+//   | | |   <| |  / /_   |  Author:          "TkrZ"         / @TkrZ
+//   |_| |_|\_\_| /____|  |  - Many thanks to Indecent Act for permission to keep this project alive! -
 
 var px;
 function countDown() {
     "use strict";
-    var i, a = 10,
-        tD = new Date(),
-        h = tD.getUTCHours(), min = tD.getUTCMinutes(), timeSec = tD.getUTCSeconds(),
-        updateEl = function (e, c) { document.getElementById(e).innerHTML = c; },
-        updateArray = function () { for (i = 6; i > 0; i -= 1) { var p = 6400, timeInMins = (h * 60) + min, tick; while (timeInMins <= p) { p -= a; }
-                                                                tick = p + a - timeInMins; if (tick < 1) { tick = a; } a *= 2; p = tick * 60 - timeSec;
-                                                                updateEl("count" + i, ("0" + Math.floor(p / 3600)).slice(-2) + ":" + ("0" + Math.floor((p % 3600) / 60)).slice(-2) + ":" + ("0" + (60 - timeSec)).slice(-2)); } };
-    updateArray();
-    updateEl("currentDate", tD.toUTCString(), h >= 12 ? "PM" : "AM");
-    px = (Math.floor(((h < 12 ? h + 48 : h) * 3600 + min * 60 + timeSec) / 30) + -405);
+    var a = 10, i, p, tick,
+        d = new Date(),
+        h6 = d.getUTCHours() * 60, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h6 + m,
+        math = function (e) { return ("0" + Math.floor(e)).slice(-2); };
+    for (i = 6; i > 0; i -= 1) { p = 6400; while (t <= p) { p -= a; } tick = p + a - t; if (tick < 1) { tick = a; } a *= 2; p = tick * 60 - s;
+                                document.getElementById("count" + i).innerHTML = math(p / 3600) + ":" + math((p % 3600) / 60) + ":" + math(60 - s); }
+    document.getElementById("currentDate").innerHTML = d.toUTCString();
+    px = Math.floor((((h6 < 12 ? h6 + 24 : h6) + m) * 60 + s) / 30) + -405;
 }
 
 document.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
