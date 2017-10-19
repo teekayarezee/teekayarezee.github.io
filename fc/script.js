982348a20e6ba7593d14308d0ea3244a267fc6af
irobot335 <sysfader@gmail.com>
2017-10-19 01:35:46 +0100

    Release Final 7

--- fc/script.js
+++ fc/script.js
@@ -1,4 +1,5 @@
 /* eslint-env browser */
+/* exported px */
 // _____ _        _____
 // |_   _| | ___ _|__  /
 //   | | | |/ / '__|/ / 
@@ -8,37 +9,21 @@
 // Farming Tick Timer/Clock/Calculator - PHP to HTML/JS Re-write
 // Many thanks to "Indecent Act" for permission
 //
-// Version 1.6c
+// Version 2.0c
 // Original Author: "Indecent Act" / @RSLinkify
 // Author:          "TkrZ" / @TkrZ
 
+var px;
 function countDown() {
     "use strict";
-    
-    var i, a = 10, b = 6,
+    var i, a = 10,
         tD = new Date(),
-        timeHour = (tD.getUTCHours() < 12) ? tD.getUTCHours() + 24 : tD.getUTCHours(),
-        timeMin = tD.getUTCMinutes(),
-        timeSec = tD.getUTCSeconds(),
-        timeInMins = (timeHour * 60) + timeMin,
+        timeHour = tD.getUTCHours() < 12 ? tD.getUTCHours() + 48 : tD.getUTCHours() + 24, timeMin = tD.getUTCMinutes(), timeSec = tD.getUTCSeconds(),
         updateEl = function (e, c) { document.getElementById(e).innerHTML = c; },
-        secs = ("0" + (60 - tD.getUTCSeconds())).slice(-2),
-        
-        updateArray = function () { for (i = 6; i > 0; i -= 1) { var p = 6400, hour, min, t, tick;
+        updateArray = function () { for (i = 6; i > 0; i -= 1) { var p = 6400, timeInMins = (timeHour * 60) + timeMin, tick;
                                                                 while (timeInMins <= p) { p -= a; }
-                                                                tick = p + a - timeInMins;
-                                                                if (tick < 1) { tick = a; }
-                                                                
-                                                                tick *= 60;
-                                                                hour = Math.floor((tick - timeSec) / 3600);
-                                                                min = Math.floor(((tick - timeSec) % 3600) / 60);
-                                                                t = ((hour * 3600) + (min * 60) + ((tick - timeSec) % 60)) * 1e3;
-                                                                t = (t > b * 1e5 || t < 0) ? b * 1e5 : t;
-                                                                
-                                                                console.log(b);
-                                                                a *= 2;
-                                                                b *= 2;
-                                                                updateEl("count" + i, ("0" + (Math.floor(t / 36e5))).slice(-2) + ":" + ("0" + (Math.floor((t % 36e5) / 6e4))).slice(-2) + ":" + secs); } };
+                                                                tick = p + a - timeInMins; if (tick < 1) { tick = a; } tick *= 60; a *= 2;
+                                                                updateEl("count" + i, ("0" + Math.floor((tick - timeSec) / 3600)).slice(-2) + ":" + ("0" + Math.floor(((tick - timeSec) % 3600) / 60)).slice(-2) + ":" + ("0" + (60 - timeSec)).slice(-2)); } };
     
     updateArray();
     updateEl("currentHour", tD.getUTCHours());
@@ -46,8 +31,7 @@ function countDown() {
     updateEl("currentSec", timeSec);
     updateEl("am_pm", tD.getUTCHours() >= 12 ? "PM" : "AM");
     updateEl("currentDate", tD.toString().split(' ').splice(1, 3).join(' '));
-    document.getElementById("icon").style.backgroundPosition = "-" + (Math.floor((timeHour * 3600 + timeMin * 60 + timeSec) / 30) + -405) + "px 0px";
+    px = (Math.floor((timeHour * 3600 + timeMin * 60 + timeSec) / 30) + -405);
 }
 
 document.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
-
