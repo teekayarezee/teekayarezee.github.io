59148f7861415075b400d35af1230259f2c65a18
irobot335 <sysfader@gmail.com>
2017-10-19 22:17:03 +0100

    hour fix

--- fc/script.js
+++ fc/script.js
@@ -11,12 +11,12 @@ function countDown() {
     "use strict";
     var a = 10, i, p, tick,
         d = new Date(),
-        h6 = d.getUTCHours() * 60, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h6 + m,
+        h = d.getUTCHours(), h6 = d.getUTCHours() * 60, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h6 + m,
         math = function (e) { return ("0" + Math.floor(e)).slice(-2); };
     for (i = 6; i > 0; i -= 1) { p = 6400; while (t <= p) { p -= a; } tick = p + a - t; if (tick < 1) { tick = a; } a *= 2; p = tick * 60 - s;
                                 document.getElementById("count" + i).innerHTML = math(p / 3600) + ":" + math((p % 3600) / 60) + ":" + math(60 - s); }
     document.getElementById("currentDate").innerHTML = d.toUTCString();
-    px = Math.floor((((h6 < 12 ? h6 + 24 : h6) + m) * 60 + s) / 30) + -405;
+    px = Math.floor((((h < 12 ? h + 24 : h) + m) * 60 + s) / 30) + -405;
 }
 
 document.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
