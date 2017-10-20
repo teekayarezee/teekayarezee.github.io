f86f9017392920d9dba4b8ecb1c0241ab072e41d
irobot335 <sysfader@gmail.com>
2017-10-20 17:59:21 +0100

    timefix

--- fc/script.js
+++ fc/script.js
@@ -6,15 +6,15 @@
 //   | | |   <| |  / /_   |  Author:          "TkrZ"         / @TkrZ
 //   |_| |_|\_\_| /____|  |  - Many thanks to Indecent Act for permission to keep this project alive! -
 
-var x;
+var x, o = document;
 function countDown() {
     "use strict";
-    var a = 10, i, p, k,
-        d = new Date(), u = d.getUTCHours(), h = u < 12 ? u + 24 : u, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h * 60 + m, f = function (e) { return e === 60 ? "00" : ("0" + Math.floor(e)).slice(-2); };
-    for (i = 6; i > 0; i -= 1) { p = 6400; while (t <= p) { p -= a; } k = p + a - t; if (k < 1) { k = a; } a *= 2; p = k * 60 - s;
-                                document.getElementById(i).innerHTML = f(p / 3600) + ":" + f((p % 3600) / 60) + ":" + f(60 - s); }
-    document.getElementById("cD").innerHTML = d.toUTCString();
-    x = (((h * 60 + m) * 60 + s) / 30) + -405;
+    var a = 10, i, p, k, d = new Date(), u = d.getUTCHours(), h = u + 24, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h * 60 + m,
+        f = function (e) { return e === 60 ? "00" : ("0" + ~~(e)).slice(-2); };
+    for (i = 6; i > 0; i -= 1) { p = 6400; while (t <= p) { p -= a; } k = p + a - t; k = k < 1 ? a : k; a *= 2; p = k * 60 - s;
+                                o.getElementById(i).innerHTML = f(p / 3600) + ":" + f((p % 3600) / 60) + ":" + f(60 - s); }
+    o.getElementById("cD").innerHTML = d.toUTCString();
+    x = ((h * 60 + m) * 60 + s) / 30 + -405;
 }
 
-document.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
+o.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
