72b70a98a70dd0f64ff88bd9234fadf8586fdb09
irobot335 <sysfader@gmail.com>
2017-10-20 22:01:23 +0100

    time fixes 2

--- fc/script.js
+++ fc/script.js
@@ -1,5 +1,4 @@
-/* eslint-env browser */
-/* exported x */
+/* eslint-env browser */ /* exported x */
 //  _____ _        _____  |  Farming Tick Timer/Clock/Calculator - PHP to HTML/JS Re-write
 // |_   _| | ___ _|__  /  |  Version 2.5a
 //   | | | |/ / '__|/ /   |  Original Author: "Indecent Act" / @RSLinkify
@@ -9,12 +8,12 @@
 var x, o = document;
 function countDown() {
     "use strict";
-    var a = 10, i, p, k, d = new Date(), u = d.getUTCHours(), h = u + 24, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h * 60 + m,
+    var a = 10, i, p, k, d = new Date(), h = d.getUTCHours() + 24, m = d.getUTCMinutes(), s = d.getUTCSeconds(), t = h * 60 + m,
         f = function (e) { return e === 60 ? "00" : ("0" + ~~(e)).slice(-2); };
     for (i = 6; i > 0; i -= 1) { p = 6400; while (t <= p) { p -= a; } k = p + a - t; k = k < 1 ? a : k; a *= 2; p = k * 60 - s;
-                                o.getElementById(i).innerHTML = f(p / 3600) + ":" + f((p % 3600) / 60) + ":" + f(60 - s); }
+                                o.getElementById(i).innerHTML = f(p / 3600) + ":" + f((p % 3600) / 60) + ":" + f(p % 60); }
     o.getElementById("cD").innerHTML = d.toUTCString();
-    x = ((h * 60 + m) * 60 + s) / 30 + -405;
+    x = ~~(((h * 60 + m) * 60 + s) / 30) + -405;
 }
 
 o.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
