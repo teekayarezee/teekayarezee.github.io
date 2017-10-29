d558b7df81814d964b7222a1a7a076fe1f16d6cd
Tom Waters <sysfader@gmail.com>
2017-10-29 02:44:38 +0000

    re-golfing

--- fc/script.js
+++ fc/script.js
@@ -1,24 +1,24 @@
-/* eslint-env browser */ /* exported x */
+/*jslint bitwise: true */ /* eslint-env browser */ /* exported x */
 //  _____ _        _____  |  Farming Tick Timer/Clock/Calculator - PHP to HTML/JS Re-write
-// |_   _| | ___ _|__  /  |  Version 4a
+// |_   _| | ___ _|__  /  |  Version 5b
 //   | | | |/ / '__|/ /   |  Original Author: "Indecent Act" / @RSLinkify
 //   | | |   <| |  / /_   |  Author:          "TkrZ"         / @TkrZ
 //   |_| |_|\_\_| /____|  |  - Many thanks to Indecent Act for permission to keep this project alive! -
 
-var x;
 function countDown() {
     "use strict";
-    var a = 10, p = Date.now() / 864e5, i = (p - ~~p) * 24, m = (i - ~~i) * 60, s = (m - ~~m) * 60, t = ~~i,
-        f = function (e) { return ("0" + ~~e).slice(-2); };
+    var p = Date.now() / 864e5, i = (p - ~~p) * 24, m = (i - ~~i) * 60, s = (m - ~~m) * 60, t = ~~i;
     
     t += ~~p / 2 === ~~(p / 2) ? t < 12 ? 48 : 0 : 24;
     t = t * 60 + ~~m;
-    x = ~~((60 * t + ~~s) / 30) + -405;
     
-    for (i = 6; i > 0; a *= 2, i--) {
-        for (p = 6400; t <= p;) { p -= a; }
-        p = 60 * (p + a - t < 1 ? a : p + a - t) - ~~s;
-        document.getElementById(i).innerHTML = f(p / 3600) + ":" + f(p % 3600 / 60) + ":" + f(p % 60); }
+    for (m = 10, i = 7; i-- > 1; m *= 2) {
+        for (p = 6400; t <= p;) { p -= m; }
+        p = 60 * (p + m - t < 1 ? m : p + m - t) - ~~s;
+        document.getElementById(i).innerHTML = new Date(1e3 * p).toISOString().substr(11, 8);
+    }
+    
+    document.getElementById("icon").style.backgroundPosition = -(~~((60 * t + ~~s) / 30) + -405) + "px 0px";
 }
 
 document.addEventListener("DOMContentLoaded", function () { "use strict"; countDown(); });
